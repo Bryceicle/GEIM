25f55a120198209196dfa99c31ea61c8fd6e9077
Bryceicle <bryce.gill@ontariotechu.net>
2026-07-27 23:52:14 -0400

    Finished adding objects to Fishermans house. Added inventory to player. Added "interact" functionality to player and objects. Added beach scene. Allowed camera to move with player. Added dialouge UI.

---
+++ geim/scenes/objects/knife.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@onready var player: CharacterBody2D = $"../../../Fisherman"
+@onready var camera: Camera2D = $"../../../Camera"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		player.interactableNode = self
+
+func interact() -> void:
+	player.addItem(2)
+	camera.showDialogue("Never know when you'll need a knife ...", 0)
+	self.queue_free()
